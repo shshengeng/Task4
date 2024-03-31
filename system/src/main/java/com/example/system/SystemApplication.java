070837b80ccf07e54e4fd05e71986e64441b599c
Brian Zhang <ericzhang64@yahoo.com>
2024-03-30 20:25:21 -0400

    update common module

--- system/src/main/java/com/example/system/SystemApplication.java
+++ system/src/main/java/com/example/system/SystemApplication.java
@@ -3,15 +3,9 @@ package com.example.system;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.time.LocalDateTime;
-
 @SpringBootApplication
 public class SystemApplication {
-
     public static void main(String[] args) {
-        LocalDateTime localDateTime = LocalDateTime.now();
-        System.out.println(localDateTime);
         SpringApplication.run(SystemApplication.class, args);
     }
-
 }
--- system/src/main/java/com/example/system/config/OpenApiConfig.java
+++ system/src/main/java/com/example/system/config/OpenApiConfig.java
@@ -1,2 +1,21 @@
-package com.example.system.config;public class OpenApiConfig {
+package com.example.system.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI openApi() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("第四次任务文档")
+                        .description("文档描述")
+                        .contact(new Contact().name("brian").email("shshengeng@gamil.com"))
+                        .version("v1.0"));
+    }
 }
